00baecf5add22031692d30bfb918af979bf4c81b
Nikola Irinchev <irinchev@me.com>
2025-04-10 12:54:28 +0200

    revert unnecessary change

--- src/state.ts
+++ src/state.ts
@@ -11,12 +11,8 @@ export interface State {
 }
 
 export async function saveState(state: State): Promise<void> {
-    const stateCopy: State = {
-        auth: state.auth,
-    };
-
     return new Promise((resolve, reject) => {
-        fs.writeFile(config.stateFile, JSON.stringify(stateCopy), function (err) {
+        fs.writeFile(config.stateFile, JSON.stringify(state), function (err) {
             if (err) {
                 return reject(err);
             }
