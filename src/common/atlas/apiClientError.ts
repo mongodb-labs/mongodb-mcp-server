949f71804284d6d93fb5c1dd006d1ed340b51b14
Filipe C Menezes <filipe.menezes@mongodb.com>
2025-04-11 15:26:29 +0100

    chore: auto generate apiClient

---
+++ src/common/atlas/apiClientError.ts
@@ -0,0 +1,19 @@
+export class ApiClientError extends Error {
+    response?: Response;
+
+    constructor(message: string, response: Response | undefined = undefined) {
+        super(message);
+        this.name = "ApiClientError";
+        this.response = response;
+    }
+
+    static async fromResponse(response: Response, message?: string): Promise<ApiClientError> {
+        message ||= `error calling Atlas API`;
+        try {
+            const text = await response.text();
+            return new ApiClientError(`${message}: [${response.status} ${response.statusText}] ${text}`, response);
+        } catch {
+            return new ApiClientError(`${message}: ${response.status} ${response.statusText}`, response);
+        }
+    }
+}
